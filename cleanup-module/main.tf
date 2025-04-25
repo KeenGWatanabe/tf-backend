27ff52e4d81814c1962576f1937fa07d9b30f1fa
keengwatanabe <mckeenweb@gmail.com>
2025-04-26 07:50:11 +0800

    chgs

--- cleanup-module/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_role_policy_attachment" "detach_example" {
-  role       = var.role_name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-
-  lifecycle {
-    prevent_destroy = false # Allows Terraform to delete
-  }
-}
