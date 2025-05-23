dba54c3197ffbe2b17eb5e28adf2f11a607f2afc
KeenGWatanabe <mckeenweb@gmail.com>
2025-05-24 05:55:05 +0800

    r

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"
 
 # 5. (Optional) State Locking with DynamoDB (Prevent conflicts)
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locks"
+  name         = "ce-grp-4-terraform-state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
